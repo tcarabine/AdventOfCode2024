2bbb5fe06102b936c369c257db73dc74164ff9b9
Tom Carabine <12697250+tcarabine@users.noreply.github.com>
2024-12-03 00:08:38 +0000

    day-02 refactor

--- day-02/src/main.rs
+++ day-02/src/main.rs
@@ -41,9 +41,21 @@ fn is_stable(left: &i32, right: &i32, comp: impl Fn(&i32,&i32) -> bool) -> bool 
     let diff = left.abs_diff(*right);
     return diff.ge(&1) && diff.le(&3)
   }
-  else {
-      false
+  return false;
+}
+
+fn is_safe(readings: &Vec<i32>) -> bool {
+  let comp = up_or_down(&readings[0], &readings[1]);
+  return readings.windows(2).all(|pair| is_stable(&pair[0],&pair[1],&comp));
+}
+
+fn can_be_made_safe(readings: &Vec<i32>) -> bool {
+  for (remove, _) in readings.iter().enumerate() {
+    let mut amended = readings.clone().clone();
+    amended.remove(remove);
+    if is_safe(&amended) {return true;}
   }
+  return false;
 }
 
 fn main() {
@@ -51,92 +63,19 @@ fn main() {
     let filename = "data/input.txt";
     let data = load_data(filename);
     
-    let mut numbers = Vec::new();
+    let numbers: Vec<Vec<i32>> = data.iter().map(|line| 
+      line.split_whitespace().map(|x| x.parse::<i32>().unwrap()).collect()
+    ).collect();
 
-    for line in data.iter() {
-      let split: Vec<i32> = line
-        .split(" ")
-        .filter(|x| !x.is_empty())
-        .map(|x| x.parse::<i32>().unwrap())
-        .collect();
-      numbers.push(split);
-    }
-
-    let part1_filtered_lines: Vec<&Vec<i32>> = numbers.iter()
-      .filter(|readings| {
-        let first = readings.get(0).unwrap();
-        let second = readings.get(1).unwrap();
-
-        if first == second { return false }
-        let cmp = up_or_down(first,second);
-        
-        let slice: &[i32] = &readings;
-        let pairs: Vec<(&i32,&i32)> = slice
-          .windows(2)
-          .map(|s| (s.get(0).unwrap(),s.get(1).unwrap()))
-          .filter(|(a,b)| is_stable(a,b,&cmp))
-          .collect();
-        
-        return pairs.len() == slice.windows(2).len()
-      })
-      .collect();
-
-    let part1 = part1_filtered_lines.len() as i32;
+    let part1 = numbers.iter().filter(|readings| is_safe(*readings)).collect::<Vec<&Vec<i32>>>().len();
     println!("Part 1: {part1}");
 
     // Part 2
 
-    let part2_filtered_lines: Vec<&Vec<i32>> = numbers.iter()
-      .filter(|readings| {
-        let first = readings.get(0).unwrap();
-        let second = readings.get(1).unwrap();
-
-        let cmp = up_or_down(first,second);
-        
-        let slice: &[i32] = &readings;
-
-        let pairs: Vec<(&i32,&i32)> = slice
-          .windows(2)
-          .map(|s| (s.get(0).unwrap(),s.get(1).unwrap()))
-          .filter(|(a,b)| is_stable(a,b,&cmp))
-          .collect();
-        
-        println!("Readings:{:?}",readings);
-        if pairs.len() == slice.windows(2).len() { println!("\tAll Good"); return true;}
-        println!("\tProblems on this line, fixing");
-        
-        let mut safe = false;
-        for (remove, _) in readings.iter().enumerate() {
-          let mut readings_mut = readings.clone().clone();
-          readings_mut.remove(remove);
-          let first = readings_mut.get(0).unwrap();
-          let second = readings_mut.get(1).unwrap();
-
-          let cmp = up_or_down(first,second);
-        
-
-          let slice: &[i32] = &readings_mut;
-          let pairs: Vec<(&i32,&i32)> = slice
-          .windows(2)
-          .map(|s| (s.get(0).unwrap(),s.get(1).unwrap()))
-          .filter(|(a,b)| is_stable(a,b,&cmp))
-          .collect();
-
-          println!("\tRemoved value at {}", remove);
-          println!("\tReadings:{:?}",readings_mut);
-          if pairs.len() == slice.windows(2).len() {
-           println!("\tFixed");
-           safe = true;
-           break;
-          }
-          println!("\tStill problems");
-        }
-        println!("\tSetting safe to {safe}");
-        return safe;
-      })
-      .collect();
-
-    let part2 = part2_filtered_lines.len() as i32;
+    let part2 = numbers.iter()
+      .filter(|readings| is_safe(*readings) || can_be_made_safe(*readings))
+      .collect::<Vec<&Vec<i32>>>()
+      .len();
     
     println!("Part 2: {part2}");
 
