fe943f5e0d336a38b08f8eaeabc677814987b94c
Tom Carabine <12697250+tcarabine@users.noreply.github.com>
2024-12-03 00:13:32 +0000

    formatting

--- day-01/src/main.rs
+++ day-01/src/main.rs
@@ -2,18 +2,17 @@ use std::fs::File;
 use std::io::{BufRead, BufReader};
 use std::thread;
 
+fn load_data(filename: &str) -> Vec<String> {
+    let file = File::open(filename).expect("Cannot open File");
 
-fn load_data(filename: &str) -> Vec<String>{
-  let file = File::open(filename).expect("Cannot open File");
-
-  let reader = BufReader::new(file);
-  let data = reader.lines();
-  let data: Vec<String> = data
-    .filter(|result| result.is_ok())
-    .map(|result| result.unwrap())
-    .filter(|line| !line.is_empty())
-    .collect();
-  return data
+    let reader = BufReader::new(file);
+    let data = reader.lines();
+    let data: Vec<String> = data
+        .filter(|result| result.is_ok())
+        .map(|result| result.unwrap())
+        .filter(|line| !line.is_empty())
+        .collect();
+    return data;
 }
 
 fn main() {
@@ -22,41 +21,47 @@ fn main() {
 
     let filename = "data/input.txt";
     let data = load_data(filename);
-    
-    let (mut left, mut right) : (Vec<i32>,Vec<i32>) = (Vec::new(), Vec::new());
+
+    let (mut left, mut right): (Vec<i32>, Vec<i32>) = (Vec::new(), Vec::new());
 
     for line in data.into_iter() {
-      let split: Vec<i32> = line
-        .split(" ")
-        .filter(|x| !x.is_empty())
-        .map(|x| x.parse::<i32>().unwrap())
-        .collect();
-      left.push(split[0]);
-      right.push(split[1]);
+        let split: Vec<i32> = line
+            .split(" ")
+            .filter(|x| !x.is_empty())
+            .map(|x| x.parse::<i32>().unwrap())
+            .collect();
+        left.push(split[0]);
+        right.push(split[1]);
     }
 
     //Unstable refers to equivelence sorting. not actual stability
-    let left_thread: thread::JoinHandle<Vec<i32>> = thread::spawn(move || { left.sort_unstable(); return left;});
-    let right_thread: thread::JoinHandle<Vec<i32>> = thread::spawn(move || { right.sort_unstable(); return right;});
+    let left_thread: thread::JoinHandle<Vec<i32>> = thread::spawn(move || {
+        left.sort_unstable();
+        return left;
+    });
+    let right_thread: thread::JoinHandle<Vec<i32>> = thread::spawn(move || {
+        right.sort_unstable();
+        return right;
+    });
 
     left = left_thread.join().unwrap();
     right = right_thread.join().unwrap();
 
-    let part1 = left.iter()
-      .zip(right.iter())
-      .map(|(l,r)| l.abs_diff(*r))
-      .reduce(|acc,d| acc + d)
-      .unwrap();
+    let part1 = left
+        .iter()
+        .zip(right.iter())
+        .map(|(l, r)| l.abs_diff(*r))
+        .reduce(|acc, d| acc + d)
+        .unwrap();
     println!("Part 1: {part1}");
 
     // Part 2
 
     let part2 = left
-      .iter()
-      .map(|value| value * (right.iter().filter(|x| *x == value).count() as i32))
-      .reduce(|acc, products| acc + products)
-      .unwrap();
-    
-      println!("Part 2: {part2}");
+        .iter()
+        .map(|value| value * (right.iter().filter(|x| *x == value).count() as i32))
+        .reduce(|acc, products| acc + products)
+        .unwrap();
 
+    println!("Part 2: {part2}");
 }
--- day-02/src/main.rs
+++ day-02/src/main.rs
@@ -1,84 +1,91 @@
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 
+fn load_data(filename: &str) -> Vec<String> {
+    let file = File::open(filename).expect("Cannot open File");
 
-fn load_data(filename: &str) -> Vec<String>{
-  let file = File::open(filename).expect("Cannot open File");
-
-  let reader = BufReader::new(file);
-  let data = reader.lines();
-  let data: Vec<String> = data
-    .filter(|result| result.is_ok())
-    .map(|result| result.unwrap())
-    .filter(|line| !line.is_empty())
-    .collect();
-  return data
+    let reader = BufReader::new(file);
+    let data = reader.lines();
+    let data: Vec<String> = data
+        .filter(|result| result.is_ok())
+        .map(|result| result.unwrap())
+        .filter(|line| !line.is_empty())
+        .collect();
+    return data;
 }
 
 fn less(left: &i32, right: &i32) -> bool {
-  left.lt(right)
+    left.lt(right)
 }
 
 fn more(left: &i32, right: &i32) -> bool {
-  left.gt(right)
+    left.gt(right)
 }
 
-fn up_or_down(left: &i32, right: &i32) -> impl Fn(&i32,&i32) -> bool {
-  if left.gt(right)
-  {
-    return more as for<'a, 'b> fn(&'a i32, &'b i32) -> bool
-  }
-  else 
-  {
-    return less as for<'a, 'b> fn(&'a i32, &'b i32) -> bool
-  }
+fn up_or_down(left: &i32, right: &i32) -> impl Fn(&i32, &i32) -> bool {
+    if left.gt(right) {
+        return more as for<'a, 'b> fn(&'a i32, &'b i32) -> bool;
+    } else {
+        return less as for<'a, 'b> fn(&'a i32, &'b i32) -> bool;
+    }
 }
 
-fn is_stable(left: &i32, right: &i32, comp: impl Fn(&i32,&i32) -> bool) -> bool {
-  let following = comp(left,right);
+fn is_stable(left: &i32, right: &i32, comp: impl Fn(&i32, &i32) -> bool) -> bool {
+    let following = comp(left, right);
 
-  if following {
-    let diff = left.abs_diff(*right);
-    return diff.ge(&1) && diff.le(&3)
-  }
-  return false;
+    if following {
+        let diff = left.abs_diff(*right);
+        return diff.ge(&1) && diff.le(&3);
+    }
+    return false;
 }
 
 fn is_safe(readings: &Vec<i32>) -> bool {
-  let comp = up_or_down(&readings[0], &readings[1]);
-  return readings.windows(2).all(|pair| is_stable(&pair[0],&pair[1],&comp));
+    let comp = up_or_down(&readings[0], &readings[1]);
+    return readings
+        .windows(2)
+        .all(|pair| is_stable(&pair[0], &pair[1], &comp));
 }
 
 fn can_be_made_safe(readings: &Vec<i32>) -> bool {
-  for (remove, _) in readings.iter().enumerate() {
-    let mut amended = readings.clone().clone();
-    amended.remove(remove);
-    if is_safe(&amended) {return true;}
-  }
-  return false;
+    for (remove, _) in readings.iter().enumerate() {
+        let mut amended = readings.clone().clone();
+        amended.remove(remove);
+        if is_safe(&amended) {
+            return true;
+        }
+    }
+    return false;
 }
 
 fn main() {
     //let filename = "data/test.txt";
     let filename = "data/input.txt";
     let data = load_data(filename);
-    
-    let numbers: Vec<Vec<i32>> = data.iter().map(|line| 
-      line.split_whitespace().map(|x| x.parse::<i32>().unwrap()).collect()
-    ).collect();
 
-    let part1 = numbers.iter().filter(|readings| is_safe(*readings)).collect::<Vec<&Vec<i32>>>().len();
+    let numbers: Vec<Vec<i32>> = data
+        .iter()
+        .map(|line| {
+            line.split_whitespace()
+                .map(|x| x.parse::<i32>().unwrap())
+                .collect()
+        })
+        .collect();
+
+    let part1 = numbers
+        .iter()
+        .filter(|readings| is_safe(*readings))
+        .collect::<Vec<&Vec<i32>>>()
+        .len();
     println!("Part 1: {part1}");
 
     // Part 2
 
-    let part2 = numbers.iter()
-      .filter(|readings| is_safe(*readings) || can_be_made_safe(*readings))
-      .collect::<Vec<&Vec<i32>>>()
-      .len();
-    
+    let part2 = numbers
+        .iter()
+        .filter(|readings| is_safe(*readings) || can_be_made_safe(*readings))
+        .collect::<Vec<&Vec<i32>>>()
+        .len();
+
     println!("Part 2: {part2}");
-
-
-    
 }
