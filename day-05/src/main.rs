4049653869e86c34d1680703a222bcc9215fd1b0
Tom Carabine <12697250+tcarabine@users.noreply.github.com.>
2024-12-05 12:59:19 +0000

    day-05 part 2

--- day-05/src/main.rs
+++ day-05/src/main.rs
@@ -1,82 +1,141 @@
-use std::collections::HashMap;
+use std::{cmp::Ordering, collections::HashMap};
 
 fn main() {
     println!("Hello, world!");
     let input: Vec<&str> = include_str!("../data/input.txt").lines().collect();
-    //let input: Vec<&str> = include_str!("../data/test.txt").lines().collect();
-    let part_1 = part_1(input);
+    // let input: Vec<&str> = include_str!("../data/test.txt").lines().collect();
+    let part_1 = part_1(&input);
 
     println!("{}", part_1);
-}
-
-fn part_1(input: Vec<&str>) -> i32 {
-    let rules = generate_rules(&input);
-    let updates = generate_updates(&input);
-
-    let valid: Vec<&Vec<i32>> = updates.iter().filter(|update| apply_rules(update, &rules)).collect();
-
-    return valid.iter().map(|update| get_middle(update)).reduce(|acc,x| acc+x).unwrap();
-}
-
-fn part_2(input: Vec<&str>) -> i32 {
-    let rules = generate_rules(&input);
-    let updates = generate_updates(&input);
-
-    let invalid: Vec<&Vec<i32>> = updates.iter().filter(|update| !apply_rules(update, &rules)).collect();
-
+    let part_2 = part_2(&input);
     
-    return 0;
+    println!("{}", part_2);
 }
 
-fn generate_rules(input: &Vec<&str>) -> HashMap<i32,Vec<i32>> {
+fn part_1(input: &Vec<&str>) -> i32 {
+    let rules = generate_rules(&input);
+    let updates = generate_updates(&input);
+
+    let valid: Vec<&Vec<i32>> = updates
+        .iter()
+        .filter(|update| apply_rules(update, &rules))
+        .collect();
+
+    return valid
+        .iter()
+        .map(|update| get_middle(update))
+        .reduce(|acc, x| acc + x)
+        .unwrap();
+}
+
+fn part_2(input: &Vec<&str>) -> i32 {
+    let rules = generate_rules(&input);
+    let updates = generate_updates(&input);
+
+    let invalid: Vec<&Vec<i32>> = updates
+        .iter()
+        .filter(|update| !apply_rules(update, &rules))
+        .collect();
+
+    return invalid
+        .iter()
+        .map(|update| fix_update(update, &rules))
+        .map(|fixed| get_middle(&fixed))
+        .reduce(|acc, x| acc + x)
+        .unwrap();
+}
+
+fn generate_rules(input: &Vec<&str>) -> HashMap<i32, Vec<i32>> {
     let mut rules = HashMap::new();
 
-    input.iter().filter(|line| line.contains("|")).for_each(|rule| {
-        let (k,v) = parse_rule(rule);
-            rules.entry(k).and_modify(|val: &mut Vec<i32>| val.push(v)).or_insert(vec![v]);
-    });
-    return rules
+    input
+        .iter()
+        .filter(|line| line.contains("|"))
+        .for_each(|rule| {
+            let (k, v) = parse_rule(rule);
+            rules
+                .entry(k)
+                .and_modify(|val: &mut Vec<i32>| val.push(v))
+                .or_insert(vec![v]);
+        });
+    return rules;
 }
 
-fn parse_rule(rule: &str) -> (i32,i32) {
+fn parse_rule(rule: &str) -> (i32, i32) {
     let mut split = rule.split("|").map(|x| x.parse::<i32>().unwrap());
-    let key =  split.next().unwrap();
+    let key = split.next().unwrap();
     let value = split.next().unwrap();
-    return (key,value)
+    return (key, value);
 }
 
 fn generate_updates(input: &Vec<&str>) -> Vec<Vec<i32>> {
     let mut updates: Vec<Vec<i32>> = Vec::new();
-    input.iter().filter(|line| line.contains(",")).for_each(|update| {
-        updates.push(parse_update(update));
-    });
-    return updates
+    input
+        .iter()
+        .filter(|line| line.contains(","))
+        .for_each(|update| {
+            updates.push(parse_update(update));
+        });
+    return updates;
 }
 
 fn parse_update(update: &str) -> Vec<i32> {
-    return update.split(",").map(|page| page.parse::<i32>().unwrap()).collect();
+    return update
+        .split(",")
+        .map(|page| page.parse::<i32>().unwrap())
+        .collect();
 }
 
-fn apply_rules(update: &Vec<i32>, rules: &HashMap<i32,Vec<i32>>) -> bool {
-    return update.iter().enumerate().all(|(ix,value)| {
-        match rules.get(value) {
+fn apply_rules(update: &Vec<i32>, rules: &HashMap<i32, Vec<i32>>) -> bool {
+    return update
+        .iter()
+        .enumerate()
+        .all(|(ix, value)| match rules.get(value) {
             None => true,
-            Some(afters) => apply_rule(ix, afters, update)
-        }
-    });
+            Some(afters) => apply_rule(ix, afters, update),
+        });
 }
 
 fn apply_rule(pos: usize, afters: &Vec<i32>, update: &Vec<i32>) -> bool {
-    return update.iter().enumerate().filter(|(_,value)| afters.contains(value)).all(|(ix,_)| ix > pos);
+    return update
+        .iter()
+        .enumerate()
+        .filter(|(_, value)| afters.contains(value))
+        .all(|(ix, _)| ix > pos);
 }
 
 fn get_middle(update: &Vec<i32>) -> i32 {
-    let middle_pos = (update.len() -1) / 2;
-    return *update.get(middle_pos).unwrap();
+    return *update.get(update.len()/ 2).unwrap();
 }
 
-fn fix_update(update: &Vec<i32>, rules: &HashMap<i32,Vec<i32>>) -> Vec<i32> {
-    return update.to_vec();
+fn fix_update(update: &Vec<i32>, rules: &HashMap<i32, Vec<i32>>) -> Vec<i32> {
+    let mut fixed = update.to_vec();
+
+    while !apply_rules(&fixed, rules) {
+        fixed.sort_by(|a,b|compare(a,b,&rules));
+    }
+
+    return fixed;
+}
+
+fn compare(a: &i32, b:&i32, rules: &HashMap<i32, Vec<i32>>) -> Ordering {
+    match rules.get(a) {
+        None => return Ordering::Equal,
+        Some(afters) => match afters.contains(b) {
+            true => return Ordering::Less,
+            false => return Ordering:: Equal
+        }
+    };
+}
+
+fn fix_with_rule(pos: usize, afters: &Vec<i32>, update: &Vec<i32>) -> Vec<i32> {
+    let mut fix = update.to_vec();
+    update
+        .iter()
+        .enumerate()
+        .filter(|(_, value)| afters.contains(value))
+        .for_each(|(ix,_)| if ix > pos {fix.swap(ix, pos); println!("{:?}", fix)});
+    return fix;
 }
 
 #[cfg(test)]
@@ -87,7 +146,7 @@ mod tests {
     fn test_input_part1() {
         let input = include_str!("../data/test.txt").lines().collect();
 
-        let result = part_1(input);
+        let result = part_1(&input);
 
         assert_eq!(result, 143);
     }
@@ -96,7 +155,7 @@ mod tests {
     fn test_input_part2() {
         let input = include_str!("../data/test.txt").lines().collect();
 
-        let result = part_2(input);
+        let result = part_2(&input);
 
         assert_eq!(result, 123);
     }
