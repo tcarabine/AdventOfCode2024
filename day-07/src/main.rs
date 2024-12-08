11250897c3748d131ccaedfb32ce5f4f74f7506f
Tom Carabine <12697250+tcarabine@users.noreply.github.com>
2024-12-08 00:10:37 +0000

    day-07 part 1

---
+++ day-07/src/main.rs
@@ -0,0 +1,85 @@
+use std::collections::HashMap;
+
+fn main() {
+    let input = include_str!("../data/input.txt");
+    //let input = include_str!("../data/test.txt");
+
+    let part1 = part_1(input);
+    println!("Part 1\t{}", part1);
+}
+
+fn parse_data(line: &str) -> (i64, Vec<i64>) {
+    let mut split = line.split(':').into_iter();
+    let key: i64 = split.next().unwrap().parse().unwrap();
+
+    let value: Vec<i64> = split
+        .next()
+        .map(|str| {
+            str.split_whitespace()
+                .map(|val| val.parse::<i64>().unwrap())
+        })
+        .unwrap()
+        .collect();
+    return (key, value);
+}
+
+fn sum(a: i64, b: i64) -> i64 {
+    return a + b;
+}
+
+fn mult(a: i64, b: i64) -> i64 {
+    return a * b;
+}
+
+fn calibrates(result: &i64, values: &Vec<i64>) -> bool {
+    // let mut operations:Vec<Vec<i64>> = vec![];
+    let mut value_iter = values.iter();
+    let mut current: Vec<i64> = vec![*value_iter.next().unwrap()];
+    let mut last: Vec<i64>;
+    while let Some(next_value) = value_iter.next() {
+        last = current.clone();
+        current = vec![];
+        for last_value in last.iter() {
+            current.push(sum(*last_value, *next_value));
+            current.push(mult(*last_value, *next_value));
+        }
+    }
+
+    return current.iter().any(|value| value == result);
+}
+
+fn part_1(input: &str) -> i64 {
+    return input
+        .lines()
+        .map(|line| parse_data(line))
+        .filter(|(key, value)| calibrates(key, value))
+        .map(|(key, _)| key)
+        .reduce(|acc, x| acc + x)
+        .unwrap();
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_parser() {
+        let input = "190: 10 19";
+        let expected_key = 190;
+        let expected_value = vec![10, 19];
+
+        let (key, value) = parse_data(input);
+        assert_eq!(key, expected_key);
+        assert_eq!(value, expected_value);
+    }
+
+    #[test]
+    fn test_part_1() {
+        let input = include_str!("../data/test.txt");
+        let data: HashMap<i64, Vec<i64>> = input.lines().map(|line| parse_data(line)).collect();
+
+        let result = part_1(data);
+
+        assert_eq!(result, 3749);
+    }
+}
